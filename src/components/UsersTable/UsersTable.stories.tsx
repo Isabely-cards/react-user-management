91c5b4d13042f0287bb6837bd368c5eb6380bf8e
Isabely-cards <isabelycrds@gmail.com>
2026-02-16 21:02:44 -0300

    feat: ajustando o storybook do componente UsersTable

--- src/components/UsersTable/UsersTable.stories.tsx
+++ src/components/UsersTable/UsersTable.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { UsersTable } from '.'
 import type { User } from '../../types/User'
+import { useState } from 'react'
 
 const users: User[] = [
   { id: '1', name: 'Maria', email: 'maria@email.com', status: 'ativo' },
@@ -16,6 +17,17 @@ export default meta
 type Story = StoryObj<typeof UsersTable>
 
 export const Default: Story = {
+  render: (args) => {
+    const [currentPage, setCurrentPage] = useState(1)
+    return (
+      <UsersTable
+        {...args}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        pageSize={5}
+      />
+    )
+  },
   args: {
     users,
     order: 'asc',
